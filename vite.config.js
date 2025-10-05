efb3987a3f90ba828479b49c14d73849b8919303
Liliana Tapia <lili.tapia@duocuc.cl>
2025-10-04 21:45:32 -0300

    chore: actualizar base de Vite para nuevo repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FRONTEND-SEM7/", // nombre EXACTO del repo
+  base: "/FRONTEND-SUM3/", // nombre EXACTO del repo
 });
